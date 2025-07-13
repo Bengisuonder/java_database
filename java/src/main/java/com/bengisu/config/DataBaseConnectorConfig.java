995ac282b07a5a43f572f4421c103cf08f432ff0
Bengisu <bengisu.161@gmail.com>
2025-07-13 21:36:51 +0300

    Java ve Veri Tabanı Temelleri

---
+++ java/src/main/java/com/bengisu/config/DataBaseConnectorConfig.java
@@ -0,0 +1,37 @@
+package com.bengisu.config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DataBaseConnectorConfig
+{
+    public static Connection connection;
+
+    public static void setConnection()
+    {
+        try
+        {
+            connection = DriverManager.getConnection(DataBaseConfig.DATABASE_URL, DataBaseConfig.DATABASE_USERNAME,
+                    DataBaseConfig.DATABASE_PASSWORD);
+        }
+        catch (Exception e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+    public static Connection getConnection()
+    {
+        return connection;
+    }
+    public static void closeConnection()
+    {
+        try
+        {
+            connection.close();
+        }
+        catch (Exception e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+}
